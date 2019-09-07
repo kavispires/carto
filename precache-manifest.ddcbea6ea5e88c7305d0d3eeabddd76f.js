eab36a7edc86ac777cb3c7b3b072162d495fc779
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2019-09-07 13:28:26 -0700

    Updates

---
+++ precache-manifest.ddcbea6ea5e88c7305d0d3eeabddd76f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e98e93dc299fe06f98d69783b4fd66",
+    "url": "/carto/index.html"
+  },
+  {
+    "revision": "f35326ae4752d064a60c",
+    "url": "/carto/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6f462913ec35a8261f23",
+    "url": "/carto/static/js/2.04bc0112.chunk.js"
+  },
+  {
+    "revision": "f35326ae4752d064a60c",
+    "url": "/carto/static/js/main.cbc76bab.chunk.js"
+  },
+  {
+    "revision": "d149d37d04a8a924a050",
+    "url": "/carto/static/js/runtime~main.86fdc106.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carto/static/media/logo.5d5d9eef.svg"
+  }
+]);
