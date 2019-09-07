1e7edbc6a9c465ce19687cf10e302de55f1a447c
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2019-09-07 16:53:15 -0700

    Fixed Card component to accept ids lower than 10

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Card = ({ id, className }) => {
+const Card = ({ id, className, prefix = 'card_' }) => {
+  const numberId = id < 10 ? `0${id}` : id;
   return (
     <img
       className={className ? className : 'card'}
-      src={`${process.env.PUBLIC_URL}/images/card_${id}.jpg`}
+      src={`${process.env.PUBLIC_URL}/images/${prefix}${numberId}.jpg`}
       alt={id}
     />
   );
