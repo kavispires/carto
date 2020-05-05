2558cd500ab3c231d934911ec59b0ceb9ae2bca4
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-05-04 21:39:45 -0700

    Created Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,30 @@ import Button from '@material-ui/core/Button';
 import logo from '../images/carto-logo.svg';
 
 import useGlobalState from '../useGlobalState';
+import { SCREENS } from '../utils/contants';
 
 const Home = () => {
   // Global States
-  const [isLoading] = useGlobalState('isLoading');
+  const [, setScreen] = useGlobalState('screen');
+
+  const handleCreateGame = () => {
+    setScreen(SCREENS.SETUP);
+  };
+
+  const handleResumeGame = () => {
+    // Do magic to resume game
+    // setScreen to appropriate screen
+  };
 
   return (
     <div className="home">
       <img className="home__logo" src={logo} alt="Bunker Mind logo" />
       <h1>Carto</h1>
       <div className="home__actions">
-        <Button variant="outlined" className="btn">
+        <Button variant="outlined" className="btn" onClick={handleCreateGame}>
           Create Game
         </Button>
-        <Button variant="outlined" className="btn">
+        <Button variant="outlined" className="btn" onClick={handleResumeGame} disabled>
           Resume Game
         </Button>
       </div>
